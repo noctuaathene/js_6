8fbcf7c24a89638f3d1d14952d80e531034adf39
Magdalena Jarošová <amnerabb@gmail.com>
2024-03-10 19:23:39 +0100

    added function max3

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log('funguju!');
+
+const contPick = document.querySelector('#skill1')
+contSkill = document.querySelector('.skill__progress');
+contChange = prompt("Zadej dovednost:")
+contSkill.textContent = (contChange + " / 100")
---
+++ script.js
@@ -0,0 +1,9 @@
+const max3 = (a, b, c) => {
+    if (a > b && a > c){
+        return a;
+    } else if (b > c) {
+        return b;
+    } else {
+        return c;
+    }
+}
