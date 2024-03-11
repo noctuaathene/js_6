9fa9ac938985efa4a9a0544e99dd1c3024c45e51
Magdalena Jarošová <amnerabb@gmail.com>
2024-03-11 09:37:01 +0100

    change to Dovednosti function

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 console.log('funguju!');
 
-const contPick = document.querySelector('#skill1')
-contSkill = document.querySelector('.skill__progress');
-contChange = prompt("Zadej dovednost:")
-contSkill.textContent = (contChange + " / 100")
+const skillHtml = prompt("Zadej úroveň dovednosti v HTML od 0 do 100:")
+const skillCss = prompt("Zadej úroveň dovednosti v CSS od 0 do 100:")
+const skillJavaScript = prompt("Zadej úroveň dovednosti v Javascript od 0 do 100:")
+
+const contHtml = document.querySelector('.skill__progress');
+contHtml.textContent = (skillHtml + " / 100")
+
+const contCss = document.querySelector('.skill__progress:nth-child(1)');
+contCss.textContent = (skillCss + " / 100")
+
+const contJavaScript = document.querySelector('.skill__progress:nth-child(2)');
+contJavaScript.textContent = (skillJavaScript + " / 100")
